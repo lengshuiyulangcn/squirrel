a60fa5a59a2717b4139874a5d2ad08883062e56b
lengshuiyulangcn <li.ning@i.mbox.nagoya-u.ac.jp>
2015-11-12 22:29:58 +0900

    add reply id

--- app/controllers/squirrel/posts_controller.rb
+++ app/controllers/squirrel/posts_controller.rb
@@ -15,11 +15,29 @@ module Squirrel
       @post.attributes = post_params
       @post.author_id = current_user.id
       if @post.save
+        flash[:notice]= "创建文章成功"
         redirect_to posts_path
       else
+        flash[:notice]= "创建文章失败"
         redirect_to :back
       end
     end
+    def edit
+      @post = Post.find(params.permit(:id)[:id])
+      @sections_with_ids = Section.all.map{|section| [section.name, section.id]}
+    end
+    def update
+      @post = Post.find(params.permit(:id)[:id])
+      @post.attributes = post_params
+      if @post.author_id == current_user.id  && @post.save
+        flash[:notice]= "修改文章成功"
+        redirect_to posts_path
+      else
+        flash[:notice]= "修改失败"
+        redirect_to :back
+      end
+    end
+
     def show
       @post = Post.find(params.permit(:id)[:id])
       @comment = Comment.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Squirrel::Engine.routes.draw do
+  root "posts#index"
   resources :posts
   resources :comments
   resources :sections
